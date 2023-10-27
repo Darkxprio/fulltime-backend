58d2e617f8ce0bd1e5822f63e5df7cf162c56e00
Jordano Cuadros <thegary9647@gmail.com>
2023-10-26 23:26:58 -0500

    feat(back-4.0): Almost done the README

--- src/api/api.controller.spec.ts
+++ src/api/api.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
+import { HttpService } from '@nestjs/axios';
 
 describe('ApiController', () => {
   let controller: ApiController;
@@ -7,6 +9,7 @@ describe('ApiController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApiController],
+      providers: [ApiService, HttpService],
     }).compile();
 
     controller = module.get<ApiController>(ApiController);
--- src/api/api.service.spec.ts
+++ src/api/api.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiService } from './api.service';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiService],
+      imports: [HttpModule],
+      providers: [ApiService, HttpService],
     }).compile();
 
     service = module.get<ApiService>(ApiService);
